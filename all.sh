5274f2b7704a83e76a717cf2bb091637f9646099
laoluotouzi <lolooo@163.com>
2025-05-16 20:53:43 +0800

    Last update time: 2025-05-16 20:53:43

--- all.sh
+++ all.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-./deploy.sh
-./gh-deploy.sh
+rm -rf ./site
+mkdocs build -c -q
+cp -f ./REAMME.md ./site/REAMME.md
+
+git pull
+git add -A
+git commit -m "Last update time: $(date +"%Y-%m-%d %H:%M:%S")"
+git push
--- gh-deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-mkdocs build -c -q
-cp -f ./REAMME.md ./site/REAMME.md
